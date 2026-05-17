1aa3286743e8a42064c82e6e90e7b81d2ee2b826
Ramakant <rvvats6366@gmail.com>
2026-05-17 15:50:05 +0530

    Create docker_push.groovy

---
+++ vars/docker_push.groovy
@@ -0,0 +1,8 @@
+def call(){
+  echo "Pushing the docker image on docker hub"
+  withCredentials([usernamePassword(credentialsId:"dockerHubCred", passwordVariable:"dockerHubPass", usernameVariable:"dockerHubUser")]){ //extracting the environment variables from jenkins credentials
+  sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPass}" //login on docker hub using credentials username and pass
+  sh " docker image tag django-image:latest ${env.dockerHubUser}/django-image:latest" //changing the name of the image as it is needed to change so that we can push it on dockerhub <username>/imageName:imageTag
+  sh "docker push ${env.dockerHubUser}/django-image:latest " //Now pushing the image on docker hub
+  echo "Image pushed successfully.."
+}
